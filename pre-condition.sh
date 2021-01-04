434c596b8a8e5c1d2921b711f7358552a62e24b6
Jimmy J. Olano S <jimmy@ks7000.net.ve>
2021-01-04 11:24:01 -0400

    First pre-condition: looking for httpd service installed at CentOS 6.

---
+++ pre-condition.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# If a command fails, exit immediately:
+set -e
+
+# Verify for httpd service installed, download and install if necessary:
+[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y httpd
