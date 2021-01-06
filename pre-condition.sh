b9e22391b7a33491faa9d76a4fbe695929b91b59
Jimmy J. Olano S <jimmy@ks7000.net.ve>
2021-01-05 23:41:24 -0400

    Looking for httpd service installed on CentOS 6.9

--- pre-condition.sh
+++ pre-condition.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Verify for httpd service installed, download and install if necessary:
-[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y httpd > verbose.txt
+#[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y httpd
 
-# Show last process
-cat verbose.txt
+# Show services installed and filter name for httpd
+rpm -qa | grep -i httpd | wc -l
