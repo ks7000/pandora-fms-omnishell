48d93fd496d29bb325be4d748512af7edcf29cf8
Jimmy J. Olano S <jimmy@ks7000.net.ve>
2021-01-05 23:56:18 -0400

    Running CentOS 6 (Docker over Ubuntu) and install httpd service with rpm

--- pre-condition.sh
+++ pre-condition.sh
@@ -4,7 +4,4 @@
 set -e
 
 # Verify for httpd service installed, download and install if necessary:
-#[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y httpd
-
-# Show services installed
-rpm -qa
+[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y --verbose httpd
