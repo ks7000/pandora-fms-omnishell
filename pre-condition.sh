b7f095dc215482bac550f3e7b6576976b31deb84
Jimmy J. Olano S <jimmy@ks7000.net.ve>
2021-01-05 23:49:55 -0400

    Show all services installed on CentOS 6.9

--- pre-condition.sh
+++ pre-condition.sh
@@ -6,8 +6,5 @@ set -e
 # Verify for httpd service installed, download and install if necessary:
 #[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y httpd
 
-# Show services installed and filter name for httpd
-rpm -qa | grep -i httpd > verbose.txt
-
-# Show result
-cat verbose.txt
+# Show services installed
+rpm -qa
