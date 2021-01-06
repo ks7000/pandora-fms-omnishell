758ab5413d9bf67e8ebbc6c7529c1e668c4287a0
Jimmy J. Olano S <jimmy@ks7000.net.ve>
2021-01-05 23:37:36 -0400

    Testing a pre-condition on CentOS 6.9 (verbose output)

--- pre-condition.sh
+++ pre-condition.sh
@@ -4,4 +4,7 @@
 set -e
 
 # Verify for httpd service installed, download and install if necessary:
-[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y httpd
+[ `rpm -qa | grep -i httpd | wc -l` -lt 1 ] || yum install -y httpd > verbose.txt
+
+# Show last process
+cat verbose.txt
